c216ba5dcbbda16a387091eda486e7c2d8fb7ce6
abhi <himeshchakrapani6@gmail.com>
2026-03-09 20:32:40 +0530

    fix: add health route

--- server/index.cjs
+++ server/index.cjs
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
+app.get("/health", (req, res) => res.json({ status: "ok" }));
 app.use("/", geofenceRoutes);
 app.use("/", emergencyRoutes);
 
